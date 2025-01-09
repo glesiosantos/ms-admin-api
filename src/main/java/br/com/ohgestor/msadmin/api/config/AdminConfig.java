97f0f3e5a4aa85555ad7e3ef60a30693d94a856d
glesio.santos <glesioss@gmail.com>
2025-01-09 16:52:35 -0300

    ajuste na classe para gerar usuário administrativo

--- src/main/java/br/com/ohgestor/msadmin/api/config/AdminConfig.java
+++ src/main/java/br/com/ohgestor/msadmin/api/config/AdminConfig.java
@@ -4,11 +4,8 @@ import br.com.ohgestor.msadmin.api.domains.Usuario;
 import br.com.ohgestor.msadmin.api.enuns.Perfil;
 import br.com.ohgestor.msadmin.api.repositories.UsuarioRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
