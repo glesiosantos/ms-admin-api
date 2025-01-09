95a8a7682700f25cd34c53ed8ecc60513fe8f5ae
glesio.santos <glesioss@gmail.com>
2025-01-09 08:12:57 -0300

    Inicializando o projeto gerencial

---
+++ src/main/java/br/com/ohgestor/msadmin/api/MsAdminApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ohgestor.msadmin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAdminApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ohgestor/msadmin/api/MsAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ohgestor.msadmin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
