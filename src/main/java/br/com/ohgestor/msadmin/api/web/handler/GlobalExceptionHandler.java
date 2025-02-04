d8980aea249945f8425772369c5c44d8ce2e3cba
Glêsio Santos <glesioss@gmail.com>
2025-02-04 19:11:22 -0300

    atualizando o as handler exceptions

--- src/main/java/br/com/ohgestor/msadmin/api/web/handler/GlobalExceptionHandler.java
+++ src/main/java/br/com/ohgestor/msadmin/api/web/handler/GlobalExceptionHandler.java
@@ -2,25 +2,12 @@ package br.com.ohgestor.msadmin.api.web.handler;
 
 import br.com.ohgestor.msadmin.api.web.responses.ErroValidacaoResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.nio.file.AccessDeniedException;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleException(Exception exception) {
-        return ResponseEntity.badRequest().build();
-    }
-
-    @ExceptionHandler({AccessDeniedException.class, UsernameNotFoundException.class})
-    public ResponseEntity<?> accessDeniedException(Exception exception) {
-        return ResponseEntity.badRequest().body("Acesso Negado");
-    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> error400(MethodArgumentNotValidException exception) {
