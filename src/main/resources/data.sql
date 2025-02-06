144c1fa504fe713269c0b81ef9b08a62c3abb2e3
Glêsio Santos <glesioss@gmail.com>
2025-02-06 20:42:10 -0300

    Configurando banco postgresql para simular o banco de produção

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
+INSERT INTO tb_usuario (avatar, nome, email, senha, ativo, perfil) VALUES
+    ('user.png', 'administrador', 'admin@ohgestor.com.br', '123456', true, 'ADMIN');
+
+
 INSERT INTO tb_cliente (cpf_cnpj, razao, nm_fantasia, proprietario, dt_vencimento, ativo, integrado, cep, logradouro, bairro,cidade, uf, estabelecimento) VALUES
     ('33074389000179','F F Nunes Comercio de Pecas e Servicos Mecanicos','Belauto Auto Center','Fábio Nunes Noranha', 0, false, true, '64010025','Av  Prefeito Freitas Neto, 38, Quadrar Lote 38','Mocambinho', 'Teresina', 'PI', 'CAU'),
     ('37541733326','Ester Sabrina Sales','Oficina "Ô Sales"','Ester Sabrina Sales', 0, false, false, '64045200','Av Noronha Almeida,575','Noivos','Teresina','PI', 'OME');
