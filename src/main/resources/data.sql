7b382141d0621269feb2d371127dd682fa5c3f62
Glêsio Santos <glesioss@gmail.com>
2025-02-18 18:03:54 -0300

    removendo arquivo de data

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO clientes (cpf_cnpj, razao, nm_fantasia, proprietario, cpf, vencimento, ativo, integrado, cep, logradouro, bairro,cidade, uf, estabelecimento) VALUES
-    ('37620840000167','GLESIO SANTOS DA SILVA 96548517334','SANTOS AUTO CENTER',NULL,null, 0, false, false, '64010530','Rua Des. Caio Oliveira, 8115','Mocambinho', 'Teresina', 'PI', 'OMC');
-
-INSERT INTO tb_cliente_contatos (cliente_id, contatos) VALUES
-    ((SELECT id FROM clientes WHERE cpf_cnpj = '37620840000167'), '8698490423');
