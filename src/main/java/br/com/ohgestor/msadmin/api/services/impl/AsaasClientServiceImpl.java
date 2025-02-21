789d8d6beeff15c2fcb5da727c511c70b39676cb
Glêsio Santos <glesioss@gmail.com>
2025-02-21 08:27:15 -0300

    Realizando o cadatro e busca do cliente na asaas

--- src/main/java/br/com/ohgestor/msadmin/api/services/impl/AsaasClientServiceImpl.java
+++ src/main/java/br/com/ohgestor/msadmin/api/services/impl/AsaasClientServiceImpl.java
@@ -4,6 +4,7 @@ import br.com.ohgestor.msadmin.api.config.AsaasConfig;
 import br.com.ohgestor.msadmin.api.domains.Cliente;
 import br.com.ohgestor.msadmin.api.domains.Pedido;
 import br.com.ohgestor.msadmin.api.services.AsaasClientService;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -13,6 +14,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class AsaasClientServiceImpl implements AsaasClientService {
@@ -26,17 +30,16 @@ public class AsaasClientServiceImpl implements AsaasClientService {
 
     @Override
     public ResponseEntity<String> cadastrarClienteAsaas(Cliente cliente) {
-
-        String request = "{"
-                + "\"name\":\"" + cliente.getRazaoSocial() + "\","
-                + "\"cpfCnpj\":\"" + cliente.getCpfOuCnpj() + "\","
-                +"}";
-
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/json");
         headers.set("access_token", asaasConfig.getAccessToken());
 
-        HttpEntity<String> entity = new HttpEntity<>(request, headers);
+        Map<String, Object> asaasCliente = new HashMap<>();
+        asaasCliente.put("name", cliente.getRazaoSocial());
+        asaasCliente.put("cpfCnpj", cliente.getCpfOuCnpj());
+        asaasCliente.put("company", cliente.getNomeFantasia());
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(asaasCliente, headers);
         return restTemplate.exchange(asaasConfig.getBaseUrl()+"/customers", HttpMethod.POST, entity, String.class);
     }
 
@@ -52,15 +55,19 @@ public class AsaasClientServiceImpl implements AsaasClientService {
 
     @Override
     public String gerarCobrancaPixAsaas(Pedido pedido) throws Exception {
-
         String customerId = null;
         String asaasCustomer = buscarClienteAsaas(pedido.getCliente()).getBody();
+        JsonNode jsonNode = mapper.readTree(asaasCustomer);
 
-        if(asaasCustomer == null) {
-            System.out.println("Vazio, cadastrar");
-            cadastrarClienteAsaas(pedido.getCliente());
+        if(jsonNode.get("totalCount").asInt() > 0 && !jsonNode.get("data").isEmpty() ) {
+            customerId = jsonNode.get("data").get(0).get("id").toString();
+            System.out.println("*** *** Customer id"+ customerId);
+        } else {
+            String criarAsaasCliente = cadastrarClienteAsaas(pedido.getCliente()).getBody();
+            JsonNode node = mapper.readTree(criarAsaasCliente);
+            System.out.println(node.get("id").toString());
+            customerId = node.get("id").toString();
         }
-        customerId = mapper.readTree(asaasCustomer).get(0).get("id").toString();
 
         return "";
     }
