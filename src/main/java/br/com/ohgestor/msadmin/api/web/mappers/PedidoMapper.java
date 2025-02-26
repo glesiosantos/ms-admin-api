60b2e1d7b7adb994c023caea5a368381a769a00f
Glêsio Santos <glesioss@gmail.com>
2025-02-26 10:46:06 -0300

    Ajuste no retorno para o front end

--- src/main/java/br/com/ohgestor/msadmin/api/web/mappers/PedidoMapper.java
+++ src/main/java/br/com/ohgestor/msadmin/api/web/mappers/PedidoMapper.java
@@ -7,17 +7,23 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PedidoMapper {
 
     @Mapping(target = "idPedido", source = "id")
+    @Mapping(target = "cliente", expression = "java(obterCliente(pedido))")
     @Mapping(target = "modulo", expression = "java(obterNomeModulo(pedido))")
     @Mapping(target = "asaasId", source = "codigoAsaasCobranca")
     @Mapping(target = "situacao", expression = "java(obterSituacao(pedido))")
     @Mapping(target = "quantidade", source = "quantidadeDeUsuarios")
     @Mapping(target = "valor", expression = "java(obterValorModulo(pedido))")
+    @Mapping(target = "total", expression = "java(calcularTotal(pedido))")
     @Mapping(target = "encodeImage", source = "qrCode")
     @Mapping(target = "payload", source = "chaveCompartilhamento")
+    @Mapping(target = "dataCriadoEm", expression = "java(converterData(pedido))")
     @Mapping(target = "expirationDate", source = "dataExpiracao")
     PedidoResponse converterModeloParaResponse(Pedido pedido);
 
@@ -26,10 +32,30 @@ public interface PedidoMapper {
     }
 
     default Double obterValorModulo(Pedido pedido) {
-        return  pedido.getCliente().getModulo() != null ? pedido.getCliente().getModulo().getPreco() : 0.0;
+        return  pedido.getCliente().getModulo() != null ?  pedido.getCliente().getModulo().getPreco() : 0.0;
     }
 
     default String obterSituacao(Pedido pedido) {
         return  pedido.getSituacao() != null ? pedido.getSituacao().name() : "";
+    }
+
+    default String obterCliente(Pedido pedido) {
+        return  pedido.getCliente() != null ? unificarDadosCliente(pedido.getCliente()) : "";
+    }
+
+    default String calcularTotal(Pedido pedido) {
+        return  pedido != null ? String.format("R$ %.2f", calcular(pedido)) : "R$ 0.00";
+    }
+
+    default String converterData(Pedido pedido) {
+        return LocalDate.ofInstant(pedido.getDataCriadoEm(), ZoneOffset.UTC).toString();
+    }
+
+    private String unificarDadosCliente(Cliente cliente) {
+        return String.format("%s - %s", cliente.getCpfOuCnpj(), cliente.getRazaoSocial());
+    }
+
+    private Double calcular(Pedido pedido) {
+        return pedido.getQuantidadeDeUsuarios() * pedido.getModulo().getPreco();
     }
 }
--- src/main/java/br/com/ohgestor/msadmin/api/web/responses/PedidoResponse.java
+++ src/main/java/br/com/ohgestor/msadmin/api/web/responses/PedidoResponse.java
@@ -1,14 +1,20 @@
 package br.com.ohgestor.msadmin.api.web.responses;
 
+import java.time.Instant;
+import java.time.LocalDate;
+
 public record PedidoResponse (
         Long idPedido,
+        String cliente,
         String asaasId,
         String situacao,
         String modulo,
         int quantidade,
-        double valor,
+        Double valor,
+        String total,
         String encodeImage,
         String payload,
+        String dataCriadoEm,
         String expirationDate
 ) {
 }
